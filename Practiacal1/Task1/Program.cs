e42ee8b0b9c9d6e89b398550dd87dc96f71767ed
Lisostav <lisostav4@gmail.com>
2025-09-17 16:17:27 +0300

    Commit

---
+++ Practiacal1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+        string GetMessage(int x)
+        {
+            if (IsEven(x))
+            {
+                return "Двері відкриваються";
+            }
+            return "Двері зачинені";
+        }
+        if (int.TryParse(Console.ReadLine(), out int enteredNumb))
+        {
+            Console.WriteLine(GetMessage(enteredNumb));
+        }
+        else
+        {
+            Console.WriteLine("Неправильний ввід");
+        }
+    }
+}
+
---
+++ Practiacal1/Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] GenerateRandomArray(int size, int min, int max)
+        {
+            int[] numbers = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(min, max);
+            }
+            return numbers;
+        }
+        int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        double GetAverage(int[] numbers)
+        {
+            int avg = GetSum(numbers) / numbers.Length;
+            return avg;
+        }
+        int GetMin(int[] numbers) {
+            int min = int.MaxValue;
+            foreach (int i in numbers)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+        int GetMax(int[] numbers)
+        {
+            int max = int.MinValue;
+            foreach (int i in numbers)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+        int InputInt()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int enteredNumb))
+                {
+                    return enteredNumb;
+                }
+                Console.WriteLine("Неправильний ввід");
+            }
+        }
+        Console.Write("Введіть довжину масиву: ");
+        int size = InputInt();
+
+        Console.Write("Введіть мінімальне значення: ");
+        int min = InputInt();
+
+        Console.Write("Введіть максимальне значення: ");
+        int max = InputInt();
+        int[] arr = GenerateRandomArray(size, min, max);
+
+    }
+}
